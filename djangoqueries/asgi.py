08beee57c39e6f34f88beac2e9568c0e8e9cd164
Paolo Melchiorre <paolo@melchiorre.org>
2020-04-27 10:03:12 +0200

    🎨️ Clean code

--- djangoqueries/asgi.py
+++ djangoqueries/asgi.py
@@ -11,7 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-# fmt: off
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangoqueries.settings")  # noqa
-# fmt: on
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangoqueries.settings")
+
 application = get_asgi_application()
