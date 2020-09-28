fab856c70c733f9c3193e681d5a81008628656c0
Paolo Melchiorre <paolo@melchiorre.org>
2020-09-28 09:10:51 +0200

    🎨️ Use pathlib in settings as Django 3.1 default project

--- djangoqueries/settings.py
+++ djangoqueries/settings.py
@@ -12,9 +12,10 @@ https://docs.djangoproject.com/en/stable/ref/settings/
 
 import os
 import string
+from pathlib import Path
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
