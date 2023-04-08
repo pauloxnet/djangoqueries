b582db5f94acbe6fc22dc6497448db8d543a3087
Paolo Melchiorre <paolo@melchiorre.org>
2023-04-08 11:02:01 +0200

    🔧 Add missing migrations

---
+++ blog/migrations/0005_alter_author_id_alter_blog_id_alter_entry_id.py
@@ -0,0 +1,31 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("blog", "0004_blog_lang"),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name="author",
+            name="id",
+            field=models.BigAutoField(
+                auto_created=True, primary_key=True, serialize=False, verbose_name="ID"
+            ),
+        ),
+        migrations.AlterField(
+            model_name="blog",
+            name="id",
+            field=models.BigAutoField(
+                auto_created=True, primary_key=True, serialize=False, verbose_name="ID"
+            ),
+        ),
+        migrations.AlterField(
+            model_name="entry",
+            name="id",
+            field=models.BigAutoField(
+                auto_created=True, primary_key=True, serialize=False, verbose_name="ID"
+            ),
+        ),
+    ]
---
+++ shows/migrations/0003_alter_band_id_alter_genre_id.py
@@ -0,0 +1,24 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("shows", "0002_trigram"),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name="band",
+            name="id",
+            field=models.BigAutoField(
+                auto_created=True, primary_key=True, serialize=False, verbose_name="ID"
+            ),
+        ),
+        migrations.AlterField(
+            model_name="genre",
+            name="id",
+            field=models.BigAutoField(
+                auto_created=True, primary_key=True, serialize=False, verbose_name="ID"
+            ),
+        ),
+    ]
